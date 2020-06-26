1aaf222d7b7d26a39bd149f13326ac445aff2d7a
exwm <thighsman@protonmail.com>
2020-06-26 15:05:42 -0400

    fix(clipper:helper_scripts:win): check if markers json passed to options helper script

--- src/clipper/win_helpers/yt_clipper_options.bat
+++ src/clipper/win_helpers/yt_clipper_options.bat
@@ -3,8 +3,13 @@ chcp 65001
 cd /D "%~dp0"
 
 .\yt_clipper.exe -h
-set /p opts="Enter any additional options: "
+    
+if exist "%~f1" (
+    set /p opts="Enter any additional options: "
+    .\yt_clipper.exe --markers-json "%~1" %opts%
+) else (
+    echo Missing markers json file
+)
 
-.\yt_clipper.exe --markers-json "%~1" %opts%
 
 pause
