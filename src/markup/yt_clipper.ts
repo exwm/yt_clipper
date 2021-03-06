59967c82e90fe2784bcb6f90e73ca3a892009d73
exwm <rjvgb@protonmail.com>
2021-03-05 22:17:59 -0500

    version: v3.7.0-beta.4.8.0

--- src/markup/yt_clipper.ts
+++ src/markup/yt_clipper.ts
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @locale       english
 // @name         yt_clipper
-// @version      3.7.0-beta.4.7.0
+// @version      3.7.0-beta.4.8.0
 // @description  Mark up YouTube videos and quickly generate clipped webms.
 // @author       elwm
 // @namespace    https://github.com/exwm
@@ -27,7 +27,7 @@
 // @grant        none
 // ==/UserScript==
 
-const __version__ = '3.7.0-beta.4.7.0';
+const __version__ = '3.7.0-beta.4.8.0';
 
 import { Chart, ChartConfiguration } from 'chart.js';
 import { html, stripIndent } from 'common-tags';
