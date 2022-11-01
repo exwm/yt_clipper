d99d6f6a8c40d88acea85869b0dc8457d10f8eb3
exwm <rjvgb@protonmail.com>
2022-10-31 23:09:59 -0400

    version: v5.10.0

--- src/clipper/yt_clipper.py
+++ src/clipper/yt_clipper.py
@@ -25,7 +25,7 @@ import certifi
 import coloredlogs
 import verboselogs
 
-__version__ = "5.9.1"
+__version__ = "5.10.0"
 
 ExtendedRealNumber = Union[int, float, Fraction]
 DictStrAny = Dict[str, Any]
