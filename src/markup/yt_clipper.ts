eadfac4f0743c445d49c1576f338611efa4f9dcd
exwm <rjvgb@protonmail.com>
2024-08-02 14:40:08 -0400

    version: v5.21.1

--- src/markup/yt_clipper.ts
+++ src/markup/yt_clipper.ts
@@ -3,8 +3,8 @@
 // BANNER GUARD
 // @locale       english
 // @name         yt_clipper
-// @version      5.21.0
-// @version      5.21.0
+// @version      5.21.1
+// @version      5.21.1
 // @description  Mark up YouTube videos and quickly generate clipped webms.
 // @author       elwm
 // @namespace    https://github.com/exwm
@@ -35,7 +35,7 @@
 // ==/UserScript==
 // BANNER GUARD
 
-const __version__ = '5.21.0';
+const __version__ = '5.21.1';
 
 import { Chart, ChartConfiguration } from 'chart.js';
 import { safeHtml, stripIndent } from 'common-tags';
